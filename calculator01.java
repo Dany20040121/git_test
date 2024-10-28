facf66b7fa2aede6841ec309f34bc5aef515a89a
Dany20040121 <bldh121@gmail.com>
2024-10-29 00:49:10 +0900

    계산기 예제 버튼 누르면 숫자 나오도록 만듬
    C, backspace 버튼 구현
    아직 계산 기능과 +/-, =버튼은 구현 못함

--- calculator01.java
+++ calculator01.java
@@ -2,6 +2,8 @@ package 계산기_만들기;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class calculator01 extends JFrame {
     JTextField cal_text;
@@ -35,6 +37,7 @@ public class calculator01 extends JFrame {
 
         Font font = new Font("아무거나", Font.BOLD, 30); // 글자 크기 설정
         cal_text.setFont(font);
+        cal_text.setEditable(false);
 
         cal_text.setPreferredSize(new Dimension(300, 80)); // 크기 고정 설정
         panel.add(cal_text);
@@ -51,6 +54,7 @@ public class calculator01 extends JFrame {
         for(int i = 0; i < buttons.length; i++) {
             buttons[i] = new JButton(button_names[i]);
             panel.add(buttons[i]);
+            buttons[i].addActionListener(new ButtonListener());
         }
         this.add(panel, BorderLayout.CENTER);
     }
@@ -60,6 +64,36 @@ public class calculator01 extends JFrame {
         panel.setBackground(Color.YELLOW);
         add(panel, BorderLayout.EAST);
     }
+
+    class ButtonListener implements ActionListener {            // 버튼 누르면 글자 출력
+        public void actionPerformed(ActionEvent e) {
+            String text = e.getActionCommand();
+            if(text.equals("C")) {
+                cal_text.setText("");
+            }
+            else if(text.equals("✏")) {
+                int t = cal_text.getText().length();
+                if(t == 0 || cal_text.getText().equals("0")) { // cal_text가 0이거나 없으면 0 으로 씀
+                    cal_text.setText("0");
+                }
+                else {
+                    String back = cal_text.getText().substring(0, t-1); //backspace 구현
+
+                    cal_text.setText(back);
+                }
+            }
+            else if(text.equals("=")) {
+
+            }
+            else if(text.equals("+/-")) {
+
+            }
+            else  {
+                cal_text.setText(cal_text.getText() + e.getActionCommand());
+            }
+        }
+    }
+
 
     void showWest() {                                           // 이하는 배경 양옆 색깔 지정 및 버튼 띄우기
         panel = new JPanel();
@@ -74,6 +108,6 @@ public class calculator01 extends JFrame {
     }
 
     public static void main(String[] args) {
-        new calculator01();
+        new calculator04();
     }
 }
